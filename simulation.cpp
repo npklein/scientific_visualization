de45a79cf23911eb2cdfa30011e21c4bb7ae9350
npklein <niekdeklein@gmail.com>
2016-02-05 18:09:57 +0100

    Change simulation into a simulation class

--- simulation.cpp
+++ simulation.cpp
@@ -1,172 +1,186 @@
-#include <rfftw.h>              //the numerical simulation FFTW library
+    #include "simulation.h"
+
+    //Default Constructor
+    Simulation::Simulation() {
+        // default values member variables
+        dt = 0.4;				//simulation time step
+        visc = 0.001;				//fluid viscosity
+        frozen = 0;               //toggles on/off the animation
+    }
+
+    //Overload Constructor
+    Simulation::Simulation(double new_dt, float new_visc, int new_frozen) {
+        // Non-default member variables
+        dt = new_dt;				//simulation time step
+        visc = new_visc;				//fluid viscosity
+        frozen = new_frozen;               //toggles on/off the animation
+    }
+
+    //Destructor
+    Simulation::~Simulation(){};
+
+    //Accessor Functions
+    int Simulation::get_frozen() const{return frozen;}
+    double Simulation::get_dt() const{return dt;}
+    float Simulation::get_visc() const{return visc;}
+    fftw_real* Simulation::get_rho() const {return rho;}
+    fftw_real* Simulation::get_rho0() const {return rho0;}
+    fftw_real* Simulation::get_vx() const {return vx;}
+    fftw_real* Simulation::get_vy() const {return vy;}
+    fftw_real* Simulation::get_vx0() const {return vx0;}
+    fftw_real* Simulation::get_vy0() const {return vy0;}
+    fftw_real* Simulation::get_fy() const {return fy;}
+    fftw_real* Simulation::get_fx() const {return fx;}
+
+    //Mutator functions
+    void Simulation::set_frozen(int new_frozen){frozen = new_frozen;}
+    void Simulation::set_dt(double new_dt){dt = new_dt;}
+    void Simulation::set_visc(float new_visc){visc = new_visc;}
+    void Simulation::set_vx(fftw_real* new_vx){vx = new_vx;}
+    void Simulation::set_vy(fftw_real* new_vy){vy = new_vy;}
+    void Simulation::set_vx0(fftw_real* new_vx0){vx0 = new_vx0;}
+    void Simulation::set_vy0(fftw_real* new_vy0){vy0 = new_vy0;}
+    void Simulation::set_fx(fftw_real* new_fx){fx = new_fx;}
+    void Simulation::set_fy(fftw_real* new_fy){fy = new_fy;}
+    void Simulation::set_rho(fftw_real* new_rho){rho = new_rho;}
+    void Simulation::set_rho0(fftw_real* new_rho0){rho0 = new_rho0;}
+    void Simulation::set_plan_rc(rfftwnd_plan new_plan_rc){plan_rc = new_plan_rc;}
+    void Simulation::set_plan_cr(rfftwnd_plan new_plan_cr){plan_cr = new_plan_cr;}
 
 
-//--- SIMULATION PARAMETERS ------------------------------------------------------------------------
-const int DIM = 100;			//size of simulation grid
-double dt = 0.4;				//simulation time step
-float visc = 0.001;				//fluid viscosity
-fftw_real *vx, *vy;             //(vx,vy)   = velocity field at the current moment
-fftw_real *vx0, *vy0;           //(vx0,vy0) = velocity field at the previous moment
-fftw_real *fx, *fy;	            //(fx,fy)   = user-controlled simulation forces, steered with the mouse
-fftw_real *rho, *rho0;			//smoke density at the current (rho) and previous (rho0) moment
-rfftwnd_plan plan_rc, plan_cr;  //simulation domain discretization
-int frozen = 0;               //toggles on/off the animation
-
-
-
-//------ SIMULATION CODE STARTS HERE -----------------------------------------------------------------
-
-//init_simulation: Initialize simulation data structures as a function of the grid size 'n'.
-//                 Although the simulation takes place on a 2D grid, we allocate all data structures as 1D arrays,
-//                 for compatibility with the FFTW numerical library.
-void init_simulation(int n)
-{
-    int i; size_t dim;
-    
-    dim     = n * 2*(n/2+1)*sizeof(fftw_real);        //Allocate data structures
-    vx       = (fftw_real*) malloc(dim);
-    vy       = (fftw_real*) malloc(dim);
-    vx0      = (fftw_real*) malloc(dim);
-    vy0      = (fftw_real*) malloc(dim);
-    dim     = n * n * sizeof(fftw_real);
-    fx      = (fftw_real*) malloc(dim);
-    fy      = (fftw_real*) malloc(dim);
-    rho     = (fftw_real*) malloc(dim);
-    rho0    = (fftw_real*) malloc(dim);
-    plan_rc = rfftw2d_create_plan(n, n, FFTW_REAL_TO_COMPLEX, FFTW_IN_PLACE);
-    plan_cr = rfftw2d_create_plan(n, n, FFTW_COMPLEX_TO_REAL, FFTW_IN_PLACE);
-    
-    for (i = 0; i < n * n; i++)                      //Initialize data structures to 0
-    { vx[i] = vy[i] = vx0[i] = vy0[i] = fx[i] = fy[i] = rho[i] = rho0[i] = 0.0f; }
-}
-
-
-//FFT: Execute the Fast Fourier Transform on the dataset 'vx'.
-//     'dirfection' indicates if we do the direct (1) or inverse (-1) Fourier Transform
-void FFT(int direction,void* vx)
-{
-    if(direction==1) rfftwnd_one_real_to_complex(plan_rc,(fftw_real*)vx,(fftw_complex*)vx);
-    else             rfftwnd_one_complex_to_real(plan_cr,(fftw_complex*)vx,(fftw_real*)vx);
-}
-
-int clamp(float x)
-{ return ((x)>=0.0?((int)(x)):(-((int)(1-(x))))); }
-
-float max(float x, float y)
-{ return x > y ? x : y; }
-
-//solve: Solve (compute) one step of the fluid flow simulation
-void solve(int n, fftw_real* vx, fftw_real* vy, fftw_real* vx0, fftw_real* vy0, fftw_real visc, fftw_real dt)
-{
-    fftw_real x, y, x0, y0, f, r, U[2], V[2], s, t;
-    int i, j, i0, j0, i1, j1;
-    
-    for (i=0;i<n*n;i++)
-    { vx[i] += dt*vx0[i]; vx0[i] = vx[i]; vy[i] += dt*vy0[i]; vy0[i] = vy[i]; }
-    
-    for ( x=0.5f/n,i=0 ; i<n ; i++,x+=1.0f/n )
-	   for ( y=0.5f/n,j=0 ; j<n ; j++,y+=1.0f/n )
-	   {
-           x0 = n*(x-dt*vx0[i+n*j])-0.5f;
-           y0 = n*(y-dt*vy0[i+n*j])-0.5f;
-           i0 = clamp(x0); s = x0-i0;
-           i0 = (n+(i0%n))%n;
-           i1 = (i0+1)%n;
-           j0 = clamp(y0); t = y0-j0;
-           j0 = (n+(j0%n))%n;
-           j1 = (j0+1)%n;
-           vx[i+n*j] = (1-s)*((1-t)*vx0[i0+n*j0]+t*vx0[i0+n*j1])+s*((1-t)*vx0[i1+n*j0]+t*vx0[i1+n*j1]);
-           vy[i+n*j] = (1-s)*((1-t)*vy0[i0+n*j0]+t*vy0[i0+n*j1])+s*((1-t)*vy0[i1+n*j0]+t*vy0[i1+n*j1]);
-       }
-    
-    for(i=0; i<n; i++)
-    for(j=0; j<n; j++)
-    {  vx0[i+(n+2)*j] = vx[i+n*j]; vy0[i+(n+2)*j] = vy[i+n*j]; }
-    
-    FFT(1,vx0);
-    FFT(1,vy0);
-    
-    for (i=0;i<=n;i+=2)
+    //init_simulation: Initialize simulation data structures as a function of the grid size 'n'.
+    //                 Although the simulation takes place on a 2D grid, we allocate all data structures as 1D arrays,
+    //                 for compatibility with the FFTW numerical library.
+    void Simulation::init_simulation(int n)
     {
-        x = 0.5f*i;
-        for (j=0;j<n;j++)
+        int i; size_t dim;
+
+        dim     = n * 2*(n/2+1)*sizeof(fftw_real);        //Allocate data structures
+        // set all the member variables of Simulation class
+        set_vx((fftw_real*) malloc(dim));
+        set_vy((fftw_real*) malloc(dim));
+        set_vx0((fftw_real*) malloc(dim));
+        set_vy0((fftw_real*) malloc(dim));
+        dim     = n * n * sizeof(fftw_real);
+        set_fx((fftw_real*) malloc(dim));
+        set_fy((fftw_real*) malloc(dim));
+        set_rho((fftw_real*) malloc(dim));
+        set_rho0((fftw_real*) malloc(dim));
+        set_plan_rc(rfftw2d_create_plan(n, n, FFTW_REAL_TO_COMPLEX, FFTW_IN_PLACE));
+        set_plan_cr(rfftw2d_create_plan(n, n, FFTW_COMPLEX_TO_REAL, FFTW_IN_PLACE));
+
+        for (i = 0; i < n * n; i++)                      //Initialize data structures to 0
+        { vx[i] = vy[i] = vx0[i] = vy0[i] = fx[i] = fy[i] = rho[i] = rho0[i] = 0.0f; }
+    }
+
+
+    //FFT: Execute the Fast Fourier Transform on the dataset 'vx'.
+    //     'dirfection' indicates if we do the direct (1) or inverse (-1) Fourier Transform
+    void Simulation::FFT(int direction,void* vx)
+    {
+        if(direction==1) rfftwnd_one_real_to_complex(plan_rc,(fftw_real*)vx,(fftw_complex*)vx);
+        else             rfftwnd_one_complex_to_real(plan_cr,(fftw_complex*)vx,(fftw_real*)vx);
+    }
+
+    int Simulation::clamp(float x)
+    { return ((x)>=0.0?((int)(x)):(-((int)(1-(x))))); }
+
+    //solve: Solve (compute) one step of the fluid flow simulation
+    void Simulation::solve(int n, fftw_real* vx, fftw_real* vy, fftw_real* vx0, fftw_real* vy0, fftw_real visc, fftw_real dt)
+    {
+        fftw_real x, y, x0, y0, f, r, U[2], V[2], s, t;
+        int i, j, i0, j0, i1, j1;
+
+        for (i=0;i<n*n;i++)
+        { vx[i] += dt*vx0[i]; vx0[i] = vx[i]; vy[i] += dt*vy0[i]; vy0[i] = vy[i]; }
+
+        for ( x=0.5f/n,i=0 ; i<n ; i++,x+=1.0f/n )
+           for ( y=0.5f/n,j=0 ; j<n ; j++,y+=1.0f/n )
+           {
+               x0 = n*(x-dt*vx0[i+n*j])-0.5f;
+               y0 = n*(y-dt*vy0[i+n*j])-0.5f;
+               i0 = clamp(x0); s = x0-i0;
+               i0 = (n+(i0%n))%n;
+               i1 = (i0+1)%n;
+               j0 = clamp(y0); t = y0-j0;
+               j0 = (n+(j0%n))%n;
+               j1 = (j0+1)%n;
+               vx[i+n*j] = (1-s)*((1-t)*vx0[i0+n*j0]+t*vx0[i0+n*j1])+s*((1-t)*vx0[i1+n*j0]+t*vx0[i1+n*j1]);
+               vy[i+n*j] = (1-s)*((1-t)*vy0[i0+n*j0]+t*vy0[i0+n*j1])+s*((1-t)*vy0[i1+n*j0]+t*vy0[i1+n*j1]);
+           }
+
+        for(i=0; i<n; i++)
+        for(j=0; j<n; j++)
+        {  vx0[i+(n+2)*j] = vx[i+n*j]; vy0[i+(n+2)*j] = vy[i+n*j]; }
+
+        FFT(1,vx0);
+        FFT(1,vy0);
+
+        for (i=0;i<=n;i+=2)
         {
-            y = j<=n/2 ? (fftw_real)j : (fftw_real)j-n;
-            r = x*x+y*y;
-            if ( r==0.0f ) continue;
-            f = (fftw_real)exp(-r*dt*visc);
-            U[0] = vx0[i  +(n+2)*j]; V[0] = vy0[i  +(n+2)*j];
-            U[1] = vx0[i+1+(n+2)*j]; V[1] = vy0[i+1+(n+2)*j];
-            
-            vx0[i  +(n+2)*j] = f*((1-x*x/r)*U[0]     -x*y/r *V[0]);
-            vx0[i+1+(n+2)*j] = f*((1-x*x/r)*U[1]     -x*y/r *V[1]);
-            vy0[i+  (n+2)*j] = f*(  -y*x/r *U[0] + (1-y*y/r)*V[0]);
-            vy0[i+1+(n+2)*j] = f*(  -y*x/r *U[1] + (1-y*y/r)*V[1]);
+            x = 0.5f*i;
+            for (j=0;j<n;j++)
+            {
+                y = j<=n/2 ? (fftw_real)j : (fftw_real)j-n;
+                r = x*x+y*y;
+                if ( r==0.0f ) continue;
+                f = (fftw_real)exp(-r*dt*visc);
+                U[0] = vx0[i  +(n+2)*j]; V[0] = vy0[i  +(n+2)*j];
+                U[1] = vx0[i+1+(n+2)*j]; V[1] = vy0[i+1+(n+2)*j];
+
+                vx0[i  +(n+2)*j] = f*((1-x*x/r)*U[0]     -x*y/r *V[0]);
+                vx0[i+1+(n+2)*j] = f*((1-x*x/r)*U[1]     -x*y/r *V[1]);
+                vy0[i+  (n+2)*j] = f*(  -y*x/r *U[0] + (1-y*y/r)*V[0]);
+                vy0[i+1+(n+2)*j] = f*(  -y*x/r *U[1] + (1-y*y/r)*V[1]);
+            }
+        }
+
+        FFT(-1,vx0);
+        FFT(-1,vy0);
+
+        f = 1.0/(n*n);
+        for (i=0;i<n;i++)
+           for (j=0;j<n;j++)
+           { vx[i+n*j] = f*vx0[i+(n+2)*j]; vy[i+n*j] = f*vy0[i+(n+2)*j]; }
+    }
+
+
+    // diffuse_matter: This function diffuses matter that has been placed in the velocity field. It's almost identical to the
+    // velocity diffusion step in the function above. The input matter densities are in rho0 and the result is written into rho.
+    void Simulation::diffuse_matter(int n, fftw_real *vx, fftw_real *vy, fftw_real *rho, fftw_real *rho0, fftw_real dt)
+    {
+        fftw_real x, y, x0, y0, s, t;
+        int i, j, i0, j0, i1, j1;
+
+        for ( x=0.5f/n,i=0 ; i<n ; i++,x+=1.0f/n )
+        for ( y=0.5f/n,j=0 ; j<n ; j++,y+=1.0f/n )
+        {
+            x0 = n*(x-dt*vx[i+n*j])-0.5f;
+            y0 = n*(y-dt*vy[i+n*j])-0.5f;
+            i0 = clamp(x0);
+            s = x0-i0;
+            i0 = (n+(i0%n))%n;
+            i1 = (i0+1)%n;
+            j0 = clamp(y0);
+            t = y0-j0;
+            j0 = (n+(j0%n))%n;
+            j1 = (j0+1)%n;
+            rho[i+n*j] = (1-s)*((1-t)*rho0[i0+n*j0]+t*rho0[i0+n*j1])+s*((1-t)*rho0[i1+n*j0]+t*rho0[i1+n*j1]);
         }
     }
-    
-    FFT(-1,vx0);
-    FFT(-1,vy0);
-    
-    f = 1.0/(n*n);
-    for (i=0;i<n;i++)
-	   for (j=0;j<n;j++)
-	   { vx[i+n*j] = f*vx0[i+(n+2)*j]; vy[i+n*j] = f*vy0[i+(n+2)*j]; }
-}
 
-
-// diffuse_matter: This function diffuses matter that has been placed in the velocity field. It's almost identical to the
-// velocity diffusion step in the function above. The input matter densities are in rho0 and the result is written into rho.
-void diffuse_matter(int n, fftw_real *vx, fftw_real *vy, fftw_real *rho, fftw_real *rho0, fftw_real dt)
-{
-    fftw_real x, y, x0, y0, s, t;
-    int i, j, i0, j0, i1, j1;
-    
-    for ( x=0.5f/n,i=0 ; i<n ; i++,x+=1.0f/n )
-    for ( y=0.5f/n,j=0 ; j<n ; j++,y+=1.0f/n )
+    //set_forces: copy user-controlled forces to the force vectors that are sent to the solver.
+    //            Also dampen forces and matter density to get a stable simulation.
+    void Simulation::set_forces(void)
     {
-        x0 = n*(x-dt*vx[i+n*j])-0.5f;
-        y0 = n*(y-dt*vy[i+n*j])-0.5f;
-        i0 = clamp(x0);
-        s = x0-i0;
-        i0 = (n+(i0%n))%n;
-        i1 = (i0+1)%n;
-        j0 = clamp(y0);
-        t = y0-j0;
-        j0 = (n+(j0%n))%n;
-        j1 = (j0+1)%n;
-        rho[i+n*j] = (1-s)*((1-t)*rho0[i0+n*j0]+t*rho0[i0+n*j1])+s*((1-t)*rho0[i1+n*j0]+t*rho0[i1+n*j1]);
+        int i;
+        for (i = 0; i < DIM * DIM; i++)
+        {
+            rho0[i]  = 0.995 * rho[i];
+            fx[i] *= 0.85;
+            fy[i] *= 0.85;
+            vx0[i]    = fx[i];
+            vy0[i]    = fy[i];
+        }
     }
-}
 
-//set_forces: copy user-controlled forces to the force vectors that are sent to the solver.
-//            Also dampen forces and matter density to get a stable simulation.
-void set_forces(void)
-{
-    int i;
-    for (i = 0; i < DIM * DIM; i++)
-    {
-        rho0[i]  = 0.995 * rho[i];
-        fx[i] *= 0.85;
-        fy[i] *= 0.85;
-        vx0[i]    = fx[i];
-        vy0[i]    = fy[i];
-    }
-}
-
-
-//do_one_simulation_step: Do one complete cycle of the simulation:
-//      - set_forces:
-//      - solve:            read forces from the user
-//      - diffuse_matter:   compute a new set of velocities
-//      - gluPostRedisplay: draw a new visualization frame
-void do_one_simulation_step(void)
-{
-    if (!frozen)
-    {
-        set_forces();
-        solve(DIM, vx, vy, vx0, vy0, visc, dt);
-        diffuse_matter(DIM, vx, vy, rho, rho0, dt);
-        glutPostRedisplay();
-    }
-}
--- visualization.cpp
+++ visualization.cpp
@@ -1,4 +1,4 @@
-
+#include "simulation.h"
 
 //--- VISUALIZATION PARAMETERS ---------------------------------------------------------------------
 int   winWidth, winHeight;      //size of the graphics window, in pixels
@@ -13,12 +13,12 @@ int   scalar_col = 0;           //method for scalar coloring
 //int   frozen = 0;               //toggles on/off the animation
 
 
-
-
+Simulation simulation;
 
 
 //------ VISUALIZATION CODE STARTS HERE -----------------------------------------------------------------
-
+float max(float x, float y)
+{ return x > y ? x : y; }
 
 //rainbow: Implements a color palette, mapping the scalar 'value' to a rainbow color RGB
 void rainbow(float value,float* R,float* G,float* B)
@@ -83,40 +83,40 @@ void direction_to_color(float x, float y, int method)
 void visualize(void)
 {
 	int        i, j, idx, idx0, idx1, idx2, idx3; double px0,py0,px1,py1,px2,py2,px3,py3;
-	fftw_real  wn = (fftw_real)winWidth / (fftw_real)(DIM + 1);   // Grid cell width
-	fftw_real  hn = (fftw_real)winHeight / (fftw_real)(DIM + 1);  // Grid cell heigh
+    fftw_real  wn = (fftw_real)winWidth / (fftw_real)(simulation.DIM + 1);   // Grid cell width
+    fftw_real  hn = (fftw_real)winHeight / (fftw_real)(simulation.DIM + 1);  // Grid cell heigh
 
 	if (draw_smoke)
 	{
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	glBegin(GL_TRIANGLES);
-	for (j = 0; j < DIM - 1; j++)			//draw smoke
+    for (j = 0; j < simulation.DIM - 1; j++)			//draw smoke
 	{
-		for (i = 0; i < DIM - 1; i++)
+        for (i = 0; i < simulation.DIM - 1; i++)
 		{
 			px0  = wn + (fftw_real)i * wn;
 			py0  = hn + (fftw_real)j * hn;
-			idx0 = (j * DIM) + i;
+            idx0 = (j * simulation.DIM) + i;
 
 			px1  = wn + (fftw_real)i * wn;
 			py1  = hn + (fftw_real)(j + 1) * hn;
-			idx1 = ((j + 1) * DIM) + i;
+            idx1 = ((j + 1) * simulation.DIM) + i;
 
 			px2  = wn + (fftw_real)(i + 1) * wn;
 			py2  = hn + (fftw_real)(j + 1) * hn;
-			idx2 = ((j + 1) * DIM) + (i + 1);
+            idx2 = ((j + 1) * simulation.DIM) + (i + 1);
 
 			px3  = wn + (fftw_real)(i + 1) * wn;
 			py3  = hn + (fftw_real)j * hn;
-			idx3 = (j * DIM) + (i + 1);
+            idx3 = (j * simulation.DIM) + (i + 1);
 
-			set_colormap(rho[idx0]);	glVertex2f(px0, py0);
-			set_colormap(rho[idx1]);	glVertex2f(px1, py1);
-			set_colormap(rho[idx2]);	glVertex2f(px2, py2);
+            set_colormap(simulation.get_rho()[idx0]);	glVertex2f(px0, py0);
+            set_colormap(simulation.get_rho()[idx1]);	glVertex2f(px1, py1);
+            set_colormap(simulation.get_rho()[idx2]);	glVertex2f(px2, py2);
 
-			set_colormap(rho[idx0]);	glVertex2f(px0, py0);
-			set_colormap(rho[idx2]);	glVertex2f(px2, py2);
-			set_colormap(rho[idx3]);	glVertex2f(px3, py3);
+            set_colormap(simulation.get_rho()[idx0]);	glVertex2f(px0, py0);
+            set_colormap(simulation.get_rho()[idx2]);	glVertex2f(px2, py2);
+            set_colormap(simulation.get_rho()[idx3]);	glVertex2f(px3, py3);
 		}
 	}
 	glEnd();
@@ -125,13 +125,13 @@ void visualize(void)
 	if (draw_vecs)
 	{
 	  glBegin(GL_LINES);				//draw velocities
-	  for (i = 0; i < DIM; i++)
-	    for (j = 0; j < DIM; j++)
+      for (i = 0; i < simulation.DIM; i++)
+        for (j = 0; j < simulation.DIM; j++)
 	    {
-		  idx = (j * DIM) + i;
-		  direction_to_color(vx[idx],vy[idx],color_dir);
+          idx = (j * simulation.DIM) + i;
+          direction_to_color(simulation.get_vx()[idx],simulation.get_vy()[idx],color_dir);
 		  glVertex2f(wn + (fftw_real)i * wn, hn + (fftw_real)j * hn);
-		  glVertex2f((wn + (fftw_real)i * wn) + vec_scale * vx[idx], (hn + (fftw_real)j * hn) + vec_scale * vy[idx]);
+          glVertex2f((wn + (fftw_real)i * wn) + vec_scale * simulation.get_vx()[idx], (hn + (fftw_real)j * hn) + vec_scale * simulation.get_vy()[idx]);
 	    }
 	  glEnd();
 	}
