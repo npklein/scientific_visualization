047870c3543afaf41bc12b1300d3f9acb2bb7461
npklein <niekdeklein@gmail.com>
2016-02-09 17:27:18 +0100

    remove windwidth/2 and winHeight/2

--- myglwidget.cpp
+++ myglwidget.cpp
@@ -65,7 +65,7 @@ void MyGLWidget::mouseMoveEvent(QMouseEvent *event)
 {
     int mx = event->x();// - lastPos.x();
     int my = event->y();// - lastPos.y();
-    simulation.drag(mx,my, DIM, winWidth/2, winHeight/2);
+    simulation.drag(mx,my, DIM, winWidth, winHeight);
     lastPos = event->pos();
 }
 
