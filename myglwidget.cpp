60b278487f30d0cf87178bb314b366f0f495fb93
freerkvandijk <freerk.van.dijk@gmail.com>
2016-04-03 00:27:09 +0200

    tmp store for merch

--- myglwidget.cpp
+++ myglwidget.cpp
@@ -174,8 +174,8 @@ void MyGLWidget::drawVelocity(fftw_real *vx, fftw_real *vy)
     for (int i = 0; i < points_x.size(); i++)
         for (int j = 0; j < points_y.size(); j++)
         {
-            i = points_x[i];
-            j = points_x[j];
+            i = &points_x[i];
+            j = &points_x[j];
             int idx = (j * DIM) + i;
             if (glyphs == "hedgehogs"){
                 if (i % (100/number_of_glyphs) == 0 && j % (100/number_of_glyphs)  == 0){
