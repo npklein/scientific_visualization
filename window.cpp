8d4f9c6523682174c28c87777c4e94d6d64b3ad3
npklein <niekdeklein@gmail.com>
2016-04-04 15:22:53 +0200

    select points working

--- window.cpp
+++ window.cpp
@@ -31,3 +31,21 @@ void Window::on_selectPoints_clicked(bool select_points)
    ui->drawingMatterCheckbox->setChecked(!select_points);
    ui->drawingHedgehogsCheckbox->setChecked(!select_points);
 }
+
+
+
+
+void Window::on_drawingHedgehogsCheckbox_clicked(bool select_drawing_hedgehogs)
+{
+   if(!select_drawing_hedgehogs){
+       ui->drawingMatterCheckbox->setChecked(!select_drawing_hedgehogs);
+   }
+}
+
+void Window::on_drawingMatterCheckbox_clicked(bool select_drawing_matter)
+{
+   if(!select_drawing_matter){
+       ui->drawingHedgehogsCheckbox->setChecked(!select_drawing_matter);
+   }
+}
+
